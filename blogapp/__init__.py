c41e674af92886e7b190a838d363ba445adb2653
John Kaganda <jkaganda@gmail.com>
2024-02-05 09:33:20 +0200

    Added the models for the user and post

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 import os
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DB_URI')
+
 
 from blogapp import routes
---
+++ blogapp/models.py
@@ -0,0 +1,33 @@
+from blogapp import db
+from sqlalchemy import Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
+    profile_pic: Mapped[str] = mapped_column(String(50), nullable=False, default="default_profile_pic.jpg")
+    password: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    # Relationship with posts
+    posts = Mapped[list["Post"]] = relationship(back_populates="author")
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(150), nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(200), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    post_pic: Mapped[str] = mapped_column(String(50), nullable=False, default="default_post_pic.jpg")
+    date_posted: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+
+    # Relationship with user
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    author: Mapped["User"] = relationship(back_populates="posts")
+
